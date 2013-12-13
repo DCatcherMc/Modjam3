b745b3edf1c3c84b0aa67c30e3ade2961a213bcd
DCatcher <daniel.catchpole@live.com>
2013-12-13 20:15:21 +0000

    Commit

--- src/main/java/net/dcatcher/modjam/DCModjam.java
+++ src/main/java/net/dcatcher/modjam/DCModjam.java
@@ -5,6 +5,7 @@ import cpw.mods.fml.common.SidedProxy;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import cpw.mods.fml.common.network.NetworkMod;
 import cpw.mods.fml.common.registry.GameRegistry;
 import net.dcatcher.modjam.blocks.DCBlocks;
 import net.dcatcher.modjam.tiles.TENode;
@@ -13,6 +14,7 @@ import net.dcatcher.modjam.util.DCCreativeTab;
 import net.minecraft.creativetab.CreativeTabs;
 
 @Mod(modid = "dcmodjam", name = "DCModJam - 3", version = "0.0.1")
+@NetworkMod(clientSideRequired = true, serverSideRequired = false)
 public class DCModjam {
 
     @Mod.Instance("dcmodjam")
--- src/main/java/net/dcatcher/modjam/render/RenderNode.java
+++ src/main/java/net/dcatcher/modjam/render/RenderNode.java
@@ -1,9 +1,14 @@
 package net.dcatcher.modjam.render;
 
 import net.dcatcher.modjam.tiles.TENode;
+import net.minecraft.block.Block;
+import net.minecraft.client.renderer.OpenGlHelper;
+import net.minecraft.client.renderer.Tessellator;
 import net.minecraft.client.renderer.tileentity.TileEntitySpecialRenderer;
+import net.minecraft.entity.Entity;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.ResourceLocation;
+import net.minecraft.world.World;
 import org.lwjgl.opengl.GL11;
 
 public class RenderNode extends TileEntitySpecialRenderer{
@@ -20,8 +25,33 @@ public class RenderNode extends TileEntitySpecialRenderer{
         GL11.glRotatef(180F, 0.0F, 0.0F, 1.0F);
         GL11.glScalef(1.3f, 1.3f, 1.3f);
 
-        ModelNode.render(0.0625F);
+        ModelNode.render(null, 0.0F, 0.0F, -0.1F, 0.0F, 0.0F, 0.0625F);
 
         GL11.glPopMatrix();
     }
+
+    /**
+    public void render(TENode teNode, World world, int x, int y, int z, Block block){
+        Tessellator tesselator = Tessellator.instance;
+
+        float f = block.getBlockBrightness(world, x, y, z);
+
+        int l = world.getLightBrightnessForSkyBlocks(x, y, z, 0);
+        tesselator.setColorOpaque_F(f, f, f);
+
+        OpenGlHelper.setLightmapTextureCoords(OpenGlHelper.lightmapTexUnit, (float)l % 65536, (float)l / 65536);
+
+        int dir = world.getBlockMetadata(x, y, z);
+
+        GL11.glPushMatrix();
+        GL11.glTranslatef(0.5F, 0, 0.5F);
+        GL11.glRotatef(dir * (-90F), 0F, 1F, 0F);
+        GL11.glTranslatef(-0.5F, 0, -0.5F);
+        bindTexture(texture);
+
+
+
+        GL11.glPopMatrix();
+    }
+       */
 }
